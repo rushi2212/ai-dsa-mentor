6bab044cb21dc9d01c42249d66facd201c5ac6cc
Rushi <rushi@example.com>
2026-01-30 21:52:28 +0530

    deploy

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="AI DSA Mentor - MongoDB", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://ai-dsa-mentor-seven.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
