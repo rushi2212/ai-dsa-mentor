9ba0c8d94ae49c4c42fe7aecd47f3853ff7c1c05
Rushi <rushi@example.com>
2026-01-30 22:02:19 +0530

    deploy

--- backend/database.py
+++ backend/database.py
@@ -9,8 +9,10 @@ db = None
 
 async def connect_to_mongo():
     global client, db
+    # mongodb_url = os.getenv(
+    #     "MONGODB_URL", "mongodb://localhost:27017/ai_dsa_mentor")
     mongodb_url = os.getenv(
-        "MONGODB_URL", "mongodb://localhost:27017/ai_dsa_mentor")
+        "MONGODB_URL", "mongodb+srv://rushikesh220703_db_user:bG7T5gucCnDzVSj5@cluster0.k36tevp.mongodb.net/?appName=Cluster0")
     client = AsyncIOMotorClient(mongodb_url)
     # Extract database name from URL or use default
     if "/" in mongodb_url and mongodb_url.split("/")[-1]:
